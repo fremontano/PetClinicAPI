5be6daef83ec20a82e6bc461cbfa691970c0a62c
freyder_montano <fremontano24@gmail.com>
2024-11-29 16:38:28 -0300

    Se creo modelo Paciente, controladores y rutas para el CRUD de pacientes, permitiendo a los veterinarios registrar y gestionar pacientes de manera segura

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import connection from './config/database.js';
 //routers
 import veterinario from './routes/veterinario.js';
+import paciente from './routes/paciente.js';
 
 
 const app = express();
@@ -18,6 +19,7 @@ connection();
 
 
 app.use('/api/veterinarios', veterinario);
+app.use('/api/pacientes', paciente);
 
 
 
